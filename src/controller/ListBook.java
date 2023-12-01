ee83a17b754ecf757b11cfeed272e3d71f7f7e30
khaint2612 <cubinchua@gmail.com>
2023-12-01 22:24:20 +0700

    edit book

--- src/controller/ListBook.java
+++ src/controller/ListBook.java
@@ -37,12 +37,12 @@ public class ListBook {
         books.add(book);
     }
 
-    public void addBookToList(List<Author> authors){
+    public void addBookToList(List<Author> authors) {
         while (true) {
             addBook(authors);
             System.out.println("Do you want continue");
             int choice = InputData.inputInteger("Enter choice", scanner);
-            if(choice != 1){
+            if (choice != 1) {
                 break;
             }
         }
@@ -53,5 +53,75 @@ public class ListBook {
             System.out.println("id: " + b.getId() + " - title: " + b.getTitle() + " - author: " + b.getAuthor()
                     + " - price: " + b.getPrice() + " - quantity: " + b.getQuantity());
         });
+    }
+
+    private Book findBook(String title) {
+        Book bookFind = books.stream().filter(b -> b.getTitle().equals(title)).findFirst().orElse(null);
+        return bookFind;
+    }
+
+    private boolean checkBookNull(Book book) {
+        if (book == null) {
+            System.out.println("Title don't find");
+        }
+        return true;
+    }
+
+    private void editBookTitle(Book bookFind) {
+        bookFind.setTitle(InputData.inputString("Enter new title:", scanner));
+    }
+
+    private void editBookQuantity(Book bookFind) {
+
+        bookFind.setQuantity(InputData.inputInteger("Enter new quantity:", scanner));
+    }
+
+    private void editBookPrice(Book bookFind) {
+
+        bookFind.setPrice(InputData.inputFloat("Enter new price:", scanner));
+    }
+
+    private void editBookAuthor(List<Author> authors, Book bookFind) {
+
+        String authorName = InputData.inputString("Enter edit author:", scanner);
+        if (!authors.stream().anyMatch(a -> a.getName().equals(authorName))) {
+            authors.add(new Author(authorName));
+            System.out.println("Added a new author");
+        }
+        bookFind.setAuthor(authorName);
+    }
+
+    public void editBook(List<Author> authors) {
+        System.out.println("Edit book");
+        do {
+            Book bookFind = findBook(InputData.inputString("Enter title need edit", scanner));
+            if (!checkBookNull(bookFind)) {
+                System.out.println("1.Title book");
+                System.out.println("2.Author name book:");
+                System.out.println("3.Quantity book:");
+                System.out.println("4.Price book:");
+                System.out.println("0.Back");
+                int choice = InputData.inputInteger("Enter choice", scanner);
+                switch (choice) {
+                    case 1:
+                        editBookTitle(bookFind);
+                        break;
+                    case 2:
+                        editBookAuthor(authors, bookFind);
+                        break;
+                    case 3:
+                        editBookQuantity(bookFind);
+                        break;
+                    case 4:
+                        editBookPrice(bookFind);
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        System.out.println("Function don't exist");
+                        break;
+                }
+            }
+        } while (true);
     }
 }
