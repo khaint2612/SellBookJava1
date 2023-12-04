b6c071111830afe8e0e7f40987a26fc132c24049
khaint2612 <cubinchua@gmail.com>
2023-12-04 18:31:20 +0700

    menu book edit 1

--- src/app/administrator/FunctionBook.java
+++ src/app/administrator/FunctionBook.java
@@ -1,18 +1,54 @@
 package app.administrator;
 
+import java.util.List;
+import java.util.Scanner;
+
+import controller.ListAuthor;
 import controller.ListBook;
+import model.Author;
+import util.InputData;
 
 public class FunctionBook {
 
-    static ListBook listBook = new ListBook();
-
+    ListBook listBook = new ListBook();
+    Scanner scanner = new Scanner(System.in);
+    ListAuthor listAuthor = new ListAuthor();
     public void MenuBook() {
-        System.out.println("Menu book");
-        System.out.println("----------");
-        System.out.println("1.New book");
-        System.out.println("2.Show list book");
-        System.out.println("3.Edit book");
-        System.out.println("4.Remove book");
+        do {
+            System.out.println("Menu book");
+            System.out.println("----------");
+            System.out.println("1.New book");
+            System.out.println("2.Show list book");
+            System.out.println("3.Edit book");
+            System.out.println("4.Remove book");
+            int choice = InputData.inputInteger("Enter choice", scanner);
+            switch (choice) {
+                case 1:
+                    listBook.addBookToList(null);
+                    break;
+                case 2:
+                    listBook.showListBook();
+                    break;
+                case 3:
+                    listBook.editBook(null);
+                    break;
+                case 4:
+                    listBook.removeBook();
+                    break;
+                case 0:
+
+                    break;
+                default:
+                    System.out.println("Enter again choice:");
+                    break;
+            }
+        } while (true);
     }
 
+    //if list author is empty, new author.
+    private void listAuthorNullIsNew(List<Author> list){
+        if(list.isEmpty()){
+            listAuthor.authorToList();
+        }
+    }
 }
